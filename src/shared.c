fc3a91f21585ef75de6faa2a7c5e2bbd275c9451
xb-bx <nazarnik050406@gmail.com>
2024-04-01 11:17:35 +0300

    Tidy up

---
+++ src/shared.c
@@ -0,0 +1,14 @@
+#include <windows.h>
+struct response_buf {
+  char *buf;
+  size_t size;
+};
+size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
+  struct response_buf *mem = (struct response_buf *)userp;
+  size_t realsize = size * nmemb;
+  mem->buf = realloc(mem->buf, mem->size + realsize + 1);
+  memcpy(&(mem->buf[mem->size]), buffer, realsize);
+  mem->size += realsize;
+  mem->buf[mem->size] = 0;
+  return realsize;
+}
